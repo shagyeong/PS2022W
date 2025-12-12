67a25ae8d6e210bb2034d4a9c228b77c7e12ddd6
shagyeong <shagyeong21@gmail.com>
2025-12-12 23:28:12 +0900

    mv

--- C/11050.c
+++
@@ -1,9 +0,0 @@
-//11050: 이항 계수 1(수학, 구현, 조합론)
-#include<stdio.h>
-int main(void){
-    int n, k; scanf("%d %d", &n, &k);
-    int ans = 1;
-    for(int i = 0; i <k; i++){ans *= n - i;}
-    for(int i = 1; i <= k; i++){ans /= i;}
-    printf("%d", ans);
-}
--- C/11654.c
+++
@@ -1,3 +0,0 @@
-//11654: 아스키 코드(구현)
-#include<stdio.h>
-int main(void){char c; scanf("%c", &c); printf("%d", c);}
