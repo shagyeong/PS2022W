ffcd8aefcb0d30a3ddf110c81ec6e39ab912bcab
shagyeong <shagyeong21@gmail.com>
2026-01-07 21:17:38 +0000

    Update: OJ 12015 1818 2565 1363 - LIS

---
+++ OJ2026W/1818.c
@@ -0,0 +1,28 @@
+// G2 1818: 책정리(LIS,이분 탐색)
+#include <stdio.h>
+
+int main(void){
+    int j; // loop variable
+    int n; scanf("%d",&n);
+    int a[n]; for(j=0;j<n;j++){scanf("%d",&a[j]);}
+    int t[n];
+    int s=0;  // length of LIS
+    int l; // left
+    int m; // middle
+    int r; // right
+    int i; // location
+
+    for(j=0;j<n;j++){
+        l=0;
+        r=s-1;
+        i=-1;
+        while(l<=r){
+            m=(l+r)/2;
+            if(t[m]<a[j]){l=m+1;}
+            else         {i=m;r=m-1;}
+        }
+        if(i==-1){t[s++]=a[j];}
+        else     {t[i]=a[j];}
+    }
+    printf("%d",n-s);
+}
---
+++ OJ2026W/2565.c
@@ -0,0 +1,50 @@
+// G5 2565: 전깃줄(정렬,LIS)
+#include<stdio.h>
+#include<stdlib.h> // qsort()
+
+struct line{
+    int l;
+    int r;
+};
+
+int compare(const void *u,const void *v);
+
+int main(void){
+    int j; // loop variable
+    int l;
+    int r;
+    int n;
+    int s=0; // length of LIS
+    int m; // middle
+    int i; // location
+    scanf("%d",&n);
+    struct line a[n];
+    struct line t[n]; // tmp: LIS
+
+    for(j=0;j<n;j++){
+        scanf("%d %d",&l,&r);
+        a[j].l=l;
+        a[j].r=r;
+    }
+    qsort(a,n,sizeof(struct line),compare);
+    
+    // 이분 탐색 LIS
+    for(j=0;j<n;j++){
+        l=0;
+        r=s-1;
+        i=-1;
+        while(l<=r){
+            m=(l+r)/2;
+            if(t[m].r<a[j].r){l=m+1;}
+            else         {i=m;r=m-1;}
+        }
+        if(i==-1){t[s++].r=a[j].r;}
+        else     {t[i].r=a[j].r;}
+    }
+    printf("%d",n-s);
+}
+int compare(const void* u,const void* v){
+    if((((struct line*)u)->l)<(((struct line*)v)->l)){return -1;}
+    if((((struct line*)u)->l)>(((struct line*)v)->l)){return 1;}
+    return 0;
+}
